339bc82a27234fe91d75f8efa2c55e5802a90868
ly son trung <trung.lyson@haravan.com>
2021-03-07 21:05:25 +0700

    init

--- userListSlice.ts
+++ userListSlice.ts
@@ -1,25 +1,63 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import * as apiClient from './apiClient';
 
 export type User = {
-    name: String
-}
+  name: {
+    first: string;
+  };
+  picture: {
+    thumbnail: string;
+  };
+};
 
 export type UserListState = {
-    user: User[]
-    loading: boolean
-    error: boolean
-}
+  users: User[];
+  loading: boolean;
+  error: boolean;
+  nextPage: number;
+};
 
 const initialState: UserListState = {
-    user: [],
-    loading: false,
-    error: false
-}
+  users: [],
+  loading: false,
+  error: true,
+  nextPage: 1,
+};
+
+export const fetchUsers = createAsyncThunk<{ users: User[] }, { page: number }>(
+  'fetchUsers',
+  async ({ page }) => {
+    const response = await apiClient.fetchUsers(page, 10);
+    if (response.kind === 'success') {
+      return {
+        users: response.body ?? [],
+      };
+    } else {
+      throw 'Error fetching users';
+    }
+  },
+);
 
 const userListSlice = createSlice({
-    name: 'userList',
-    initialState: initialState,
-    reducers: {},
+  name: 'userList',
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.nextPage += 1;
+        state.users = state.users.concat(action.payload.users);
+        state.loading = false;
+      })
+      .addCase(fetchUsers.rejected, (state) => {
+        state.error = true;
+        state.loading = false;
+      });
+  },
 });
 
 export default userListSlice.reducer;
